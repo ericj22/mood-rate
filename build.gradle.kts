d709e3ac5e28baea37e36f0b3efec64735eb960c
Eric <eric868.ji@gmail.com>
2023-09-01 20:22:01 -0400

    Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    extra.apply {
+        set("room_version", "2.5.2")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
